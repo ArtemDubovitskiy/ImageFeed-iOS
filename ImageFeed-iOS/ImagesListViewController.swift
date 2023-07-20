d76ab111c0f2017a5e32f47722d2a6fa0f0b0d33
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-07-20 21:32:47 +0300

    Определены свойства UITableViewDelegate и UITableViewDataSource

--- ImageFeed-iOS/ImagesListViewController.swift
+++ ImageFeed-iOS/ImagesListViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Artem Dubovitsky on 04.04.2023.
 //
-
 import UIKit
 
 class ImagesListViewController: UIViewController {
@@ -12,9 +11,24 @@ class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     override func viewDidLoad() {
+//        tableView.delegate = self
+//        tableView.dataSource = self
         super.viewDidLoad()
-//        sleep(3)
-        // Do any additional setup after loading the view.
     }
 }
 
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
