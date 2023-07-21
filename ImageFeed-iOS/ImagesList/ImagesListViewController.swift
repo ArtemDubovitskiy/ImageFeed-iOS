fbb0b06b63c58c888ba62afabafd72dcf3d5a425
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-07-21 13:07:45 +0300

    Настроена передача мок данных (изображений) в таблицу

--- ImageFeed-iOS/ImagesList/ImagesListViewController.swift
+++ ImageFeed-iOS/ImagesList/ImagesListViewController.swift
@@ -11,20 +11,41 @@ class ImagesListViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet private var tableView: UITableView!
     
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "ru_Ru")
+        return formatter
+    }()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
     // MARK: - Extensions
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWight = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWight = image.size.width
+        let scale = imageViewWight / imageWight
+        let cellHight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHight
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,12 +54,23 @@ extension ImagesListViewController: UITableViewDataSource {
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
+        
         return imageListCell
     }
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell) {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLaked = indexPath.row % 2 == 0
+        let likeImage = isLaked ? UIImage(named: "like_button_active") : UIImage(named: "like_button_no_active")
+        cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
