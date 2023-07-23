f77b1bc2ca60994314fd5b79fef9926ea7f105fd
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-07-23 15:44:41 +0300

    Добавлен экран профиля пользователя

---
+++ ImageFeed-iOS/Profile/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed-iOS
+//
+//  Created by Artem Dubovitsky on 23.07.2023.
+//
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+}
